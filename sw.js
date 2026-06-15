df970f5d530a3430a3099c6a61c67a667699c27f
wasabilee714 <71054481+wasabilee714@users.noreply.github.com>
2026-06-15 19:04:36 +0800

    Add files via upload
    Add Wasabi English mobile app

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "wasabi-policy-english-v2";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./wasabi-cards.html",
+  "./manifest.webmanifest",
+  "./wasabi-icon.svg",
+  "./wasabi-icon-192.png",
+  "./wasabi-icon-512.png",
+  "./wasabi-cloud-config.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then(response => response || caches.match("./wasabi-cards.html")))
+  );
+});
---
+++ wasabi-cloud-config.js
@@ -0,0 +1,4 @@
+window.WASABI_CLOUD = {
+  supabaseUrl: "https://jvgotggtmsxmpnozapgi.supabase.co",
+  supabaseAnonKey: "sb_publishable_YHITsu_xrp4bEYsn3LqbjA_2j6_Dwr8"
+};
